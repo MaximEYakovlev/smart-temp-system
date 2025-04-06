ec769eb01a542bbe9d960246f865c49324d7e16b
Maxim Yakovlev <it.trainee@yandex.ru>
2025-04-07 02:52:18 +0300

    updated TemperatureModule, imported TypeOrmModule

--- services/backend-api/src/temperature/temperature.module.ts
+++ services/backend-api/src/temperature/temperature.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TemperatureService } from './temperature.service';
 import { TemperatureController } from './temperature.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TemperatureEntity } from './temperature.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TemperatureEntity])],
   providers: [TemperatureService],
   controllers: [TemperatureController]
 })
