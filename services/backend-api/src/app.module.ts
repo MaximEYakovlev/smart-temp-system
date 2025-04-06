673e49dce7345da3801e3c2acd0d10b055d5b9a3
Maxim Yakovlev <it.trainee@yandex.ru>
2025-04-07 01:10:39 +0300

    configured the TypeOrmModule in the AppModule

--- services/backend-api/src/app.module.ts
+++ services/backend-api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TemperatureModule } from './temperature/temperature.module';
+import { TemperatureEntity } from './temperature/temperature.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TemperatureModule } from './temperature/temperature.module';
 
 @Module({
-  imports: [TemperatureModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'my_password',
+      database: 'smart_temp',
+      entities: [TemperatureEntity],
+      synchronize: true,
+    }),
+    TemperatureModule],
   controllers: [AppController],
   providers: [AppService],
 })
