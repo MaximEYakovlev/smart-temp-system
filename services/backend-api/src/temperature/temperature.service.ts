a854f8225921342d949f627a89eec6e442250bb1
Maxim Yakovlev <it.trainee@yandex.ru>
2025-04-08 09:05:54 +0300

    updated temperature.service.ts, added websocket broadcast

--- services/backend-api/src/temperature/temperature.service.ts
+++ services/backend-api/src/temperature/temperature.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TemperatureEntity } from './temperature.entity';
 import { Repository } from 'typeorm';
+import { TemperatureGateway } from './temperature.gateway';
 
 @Injectable()
 export class TemperatureService {
   constructor(
     @InjectRepository(TemperatureEntity)
     private readonly repo: Repository<TemperatureEntity>,
+    private readonly gateway: TemperatureGateway,
   ) {}
 
   async save(value: number) {
     const entity = this.repo.create({ value });
     await this.repo.save(entity);
+    this.gateway.sendTemperature(value);
     return { status: 'ok', value };
   }
 
